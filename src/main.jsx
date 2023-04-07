5d9f02ec3f05e2140b8d75deb9c3d1003aa99fea
johurulhque <johurulhque.cse@gmail.com>
2023-04-07 17:48:51 +0600

    Loading Data

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Root from "./routes/root";
 import Contact from "./routes/contact";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './error-page';
+import Root, { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
